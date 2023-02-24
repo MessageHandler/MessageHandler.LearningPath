ca7133f34d905d2f2963c4d770e535b0727a9683
Yves Goeleven <yves@goeleven.com>
2023-02-25 00:07:56 +0100

    split files

--- 11 automated task processing/OrderBooking.Worker/SendConfirmationMail/SetConfirmationMailAsPending.cs
+++ 11 automated task processing/OrderBooking.Worker/SendConfirmationMail/SetConfirmationMailAsPending.cs
@@ -4,30 +4,30 @@ using OrderBooking.Events;
 
 namespace OrderBooking.Worker
 {
-    public class IndexConfirmationMail : IHandle<BookingStarted>
+    public class SetConfirmationMailAsPending : IHandle<SalesOrderConfirmed>
     {
         private readonly IPersistConfirmationMails _client;
         private readonly IInvokeProjections<ConfirmationMail> _projection;
         private readonly ILogger<IndexConfirmationMail> _logger;
 
-        public IndexConfirmationMail(IInvokeProjections<ConfirmationMail> projection, IPersistConfirmationMails client, ILogger<IndexConfirmationMail> logger = null!)
+        public SetConfirmationMailAsPending(IInvokeProjections<ConfirmationMail> projection, IPersistConfirmationMails client, ILogger<IndexConfirmationMail> logger = null!)
         {
             _client = client;
             _projection = projection;
             _logger = logger;
         }
 
-        public async Task Handle(BookingStarted message, IHandlerContext context)
+        public async Task Handle(SalesOrderConfirmed message, IHandlerContext context)
         {
-            _logger?.LogInformation("Received BookingStarted, indexing the confirmation mail...");
+            _logger?.LogInformation("Received SalesOrderConfirmed, marking the confirmation mail as pending...");
 
             var confirmationMail = await _client.GetConfirmationMail(message.BookingId) ?? new ConfirmationMail();
 
             _projection.Invoke(confirmationMail, message);
 
-            await _client.Persist(confirmationMail);
+            await _client.MarkAsPending(confirmationMail);
 
-            _logger?.LogInformation("Confirmation mail indexed");
+            _logger?.LogInformation("Sales order marked as pending");
         }
     }
 }
